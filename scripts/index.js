d72daaba3e9c1c0c8137570dc330dceb6eae42fe
federicorojas11 <federico.rojas.121@gmail.com>
2021-05-06 12:47:57 -0300

    desarrollo js pagina index v0.1

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,42 @@
-"use strict";
-console.log('compila');
+$(document).ready(function () {
+  var i = 0;
+  colors = [
+    "wheat",
+    "background: linear-gradient(#eeeeee, rgb(18, 8, 19))",
+    "DarkSalmon",
+    "background: linear-gradient(rgb(18, 8, 19), #eeeeee)",
+  ];
+  $("body").css({
+    background:
+      "linear-gradient(rgba(165, 71, 71, 0.5), rgba(128, 224, 109, 0.5))",
+  });
+
+  let backgroundColor = () => {
+    switch (i) {
+      case 0:
+        $("body").css({
+          background: colors[i],
+        });
+        i++;
+        break;
+      case 1:
+        $("body").css({
+          background: colors[i],
+        });
+        i++;
+        break;
+      case 2:
+        $("body").css({
+          background: colors[i],
+        });
+        i++;
+        break;
+      case 3:
+        $("body").css({
+          background: colors[i],
+        });
+        i = 0;
+        break;
+    }
+  };
+});
